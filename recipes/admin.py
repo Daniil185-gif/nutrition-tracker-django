985c114a1d9d9abf900ff0e78bfa6fa7fafef621
lexys107 <lesha.sedykh.0524@mail.ru>
2026-05-03 21:26:45 +0300

    Добавлена админка рецептов

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, RecipeIngredient
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title', 'author__username')
+    list_filter = ('created_at',)
+    inlines = (RecipeIngredientInline,)
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'product', 'grams')
+    search_fields = ('recipe__title', 'product__name')
